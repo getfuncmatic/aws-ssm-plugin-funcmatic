6597ea72f6f8cb06e7d64a325606e15fa0bd6d14
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-09-15 11:03:12 -0700

    used dotenv and funcmatic 2.0 plugin format

--- test/ssmstore.test.js
+++ test/ssmstore.test.js
@@ -1,26 +1,53 @@
-var Funcmatic = require('@funcmatic/funcmatic')
+require('dotenv').config()
+var funcmatic = require('@funcmatic/funcmatic')
 var SSMStorePlugin = require('../lib/ssmstore')
 
-
-var handler = Funcmatic.wrap(async (event, { ssm }) => {
-  console.log("SSM", ssm)
-  return {
-    statusCode: 200
-  }
-})
-
 describe('Request', () => {
-  beforeEach(() => {
-    Funcmatic.clear()
+  beforeEach(async () => {
+    funcmatic = funcmatic.clone()
+    console.log("getplugins", funcmatic.plugins)
   })
-  it ('should send a papertrail log event', async () => {
-    Funcmatic.use(SSMStorePlugin, { 
+  afterEach(async () => {
+    await funcmatic.teardown()
+    funcmatic.clear()
+  })
+  it ('IAM external-user should not have permission /Funcmatic/*', async () => {
+    funcmatic.use(SSMStorePlugin, {
       path: '/Funcmatic/dev/SSMStorePlugin',
-      region: 'us-west-2'
+      accessKeyId: process.env.accessKeyId,
+      secretAccessKey: process.env.secretAccessKey,
+      region: process.env.region,
     })
     var event = { }
     var context = { }
-    var ret = await handler(event, context)
-    console.log("RET", ret)
+    var error = null
+    try {
+      await funcmatic.invoke(event, context, async (event, context, { ssm }) => {
+        // SSMStorePulgin should throw error in start before we get here
+      })  
+    } catch (err) {
+      error = err 
+    }
+    expect(error.code).toBe("AccessDeniedException")
+  })
+  it ('IAM external-user should get parameters from /External/*', async () => {
+    funcmatic.use(SSMStorePlugin, {
+      path: '/External/ssmplugin/DEV',
+      accessKeyId: process.env.accessKeyId,
+      secretAccessKey: process.env.secretAccessKey,
+      region: process.env.region,
+    })
+    var plugin = funcmatic.getPlugin('ssm')
+    var event = { }
+    var context = { }
+    var error = null
+
+    await funcmatic.invoke(event, context, async (event, context, { ssm }) => {
+      expect(ssm).toMatchObject({
+        VARIABLE_A: 'value.a',
+        VARIABLE_B: 'value.b'
+      })
+      expect(process.env).toMatchObject(ssm)
+    })  
   })
 }) 
